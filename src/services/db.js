267a90ee6340e2973ca89e767b30236ba8c4f5f8
Shaun Tan <stqx07.23@ichat.sp.edu.sg>
2025-03-23 01:12:50 +0800

    Altered db.js

--- src/services/db.js
+++ src/services/db.js
@@ -1,16 +1,20 @@
-require('dotenv').config(); //read .env file and set environment variables
+require("dotenv").config(); //read .env file and set environment variables
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const setting = {
-    connectionLimit : 10, //set limit to 10 connection
-    host     : process.env.DB_HOST, //get host from environment variable
-    user     : process.env.DB_USER, //get user from environment variable
-    password : process.env.DB_PASSWORD, //get password from environment variable
-    database : process.env.DB_DATABASE, //get database from environment variable
-    multipleStatements: true, //allow multiple sql statements
-    dateStrings: true //return date as string instead of Date object
-}
+  connectionLimit: 10, //set limit to 10 connection
+  host: process.env.DB_HOST, //get host from environment variable
+  user: process.env.DB_USER, //get user from environment variable
+  password: process.env.DB_PASSWORD, //get password from environment variable
+  database: process.env.DB_DATABASE, //get database from environment variable
+  multipleStatements: true, //allow multiple sql statements
+  dateStrings: true, //return date as string instead of Date object
+  ssl: {
+    rejectUnauthorized: true,
+    ca: require("fs").readFileSync("./ca.pem"), // path to your CA cert from Aiven
+  },
+};
 
 const pool = mysql.createPool(setting);
 
